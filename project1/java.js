d46c375f1e409f7a369707e9cb901919ad475978
Boluwatife <gbadamosiboluwatife2002@gmail.com>
2025-09-30 15:37:00 +0100

    presentation on freshbite

---
+++ project1/java.js
@@ -0,0 +1,17 @@
+let loginBtn = document.getElementById("login-button");
+let user = "admin";
+let pass = "password";
+loginBtn.addEventListener("click", function stafflogin(){
+    let username= document.getElementById("username").value.trim();
+    let password= document.getElementById("password").value.trim();
+
+    if (username ==="" || password ==="" ){
+        alert("This fields cannot be empty!!!")
+    } else if(username === user && password === pass){
+        window.location.href = "../staff_dashboard.html"
+        alert("Password accepted! Logged in!!!")
+    } else {
+        alert("Incorrect username or password!")
+    }
+});
+// /window.location.js/ 
