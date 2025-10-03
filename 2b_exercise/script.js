d0ebc7e00f81efbbbaae1eb61716b060ad5cf830
Boluwatife <gbadamosiboluwatife2002@gmail.com>
2025-10-03 11:20:15 +0100

    2b initial commit

--- 2b_exercise/script.js
+++ 2b_exercise/script.js
@@ -1,7 +1,7 @@
 function isStrongPassword(password) {
     if (password.length < 8) {
         return "Password too short! Must be at least 8 characters.";
-    } else if (/\d/.test(password === null)) {
+    } else if (/\d+/.test(password)) {
         return "Password must contain a number.";
     } else if (password.length > 20) {
         return "Password too long! Keep it under 20 characters.";
@@ -30,4 +30,33 @@ function formatPercentage(value){
 console.log(formatPercentage(0.45));
 
 //3
- 
+
+ function calculatorCompoundInterest(principal, rate, years){
+     let ratess = 1+ rate
+     let interest= principal * (ratess**years)   
+     return `A = ${interest}`;
+}
+    console.log(calculatorCompoundInterest(1.3, 50, 2));
+
+//4
+function  canGraduate(credits, gpa){
+    if (credits >= 120 && gpa >= 2.0 ){
+        return `You have a score of ${credits} and  GPA of ${gpa}, Congratulations!! you are eligible to graduate.`
+    }
+    return `Ineligible`
+}
+ console.log(canGraduate(200, 2.2));
+ console.log("");
+ console.log(canGraduate(120, 0.2));
+
+ //5
+
+let reversedWords = (sentence)=> {
+    let reversed = "";
+    mySentence = sentence.split()
+    if (sentence !== null){
+        return `${sentence}` + `${reversed}`
+    }
+};
+console.log(reversedWords("My name is Bolu"))
+
